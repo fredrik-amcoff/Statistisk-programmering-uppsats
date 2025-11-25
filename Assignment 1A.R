8cc4c5b63881cc5eb805ab35c3e259df0e505bce
Fredrik Amcoff <fredrik.amcoff@gmail.com>
2025-11-25 14:01:50 +0100

    function

--- Assignment 1A.R
+++ Assignment 1A.R
@@ -11,24 +11,43 @@ data_frame <- tibble(
 groups <- c("A", "A", "B", "A", "B", "B", "B", "A", "B", "B")
 
 
-Welch_t_test <- function(data, group, names, alpha, bonferroni) {
-  vars_a <- list()
-  vars_b <- list()
-  n <- length(data[[1]])
-  print(n)
+Welch_t_test <- function(data, group, names, alpha=0.05, bonferroni=FALSE) {
   n_variables <- length(names)
-  groups <- ifelse(group=="A", 1, 0)
-  groups_b <- ifelse(group=="B", 1, 0)
+
+  t_vector <- numeric(n_variables)
+  p_vector <- numeric(n_variables)
 
   for (idx in 1:n_variables) {
     variable <- data[[idx]]
+
     variable_a <- variable[group == "A"]
     variable_b <- variable[group == "B"]
-    #print(variable)
-    #print(groups)
-    print(variable_a)
-    print(variable_b)
+
+    n_a <- length(variable_a)
+    n_b <- length(variable_b)
+
+    mean_a <- sum(variable_a)/n_a
+    mean_b <- sum(variable_b)/n_b
+
+    variance_a  <- sum((variable_a - mean_a)^2)/(n_a-1)
+    variance_b  <- sum((variable_b - mean_b)^2)/(n_b-1)
+
+    t <- (mean_a - mean_b)/sqrt((variance_a/n_a)+(variance_b/n_b))
+
+    df <- (((variance_a/n_a)+(variance_b/n_b))^2)/(((variance_a/n_a)^2)/(n_a-1)+((variance_b/n_b)^2)/(n_b-1))
+
+    p <- pt(t, df)
+
+    if (bonferroni == TRUE) {
+      p <- min(1, n_variables * p)
+    }
+
+    t_vector[idx] <- t
+    p_vector[idx] <- p
   }
+
+  return(list(t=t_vector, p=p_vector))
 }
 
-Welch_t_test(df, groups, c("x", "y"), 1, 1)
+obj <- Welch_t_test(data_frame, groups, c("x", "y", "z"), bonferroni=TRUE)
+
