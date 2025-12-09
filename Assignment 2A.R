c0b600cf158d578e8c1c053432f5ff78e803ad4a
Fredrik Amcoff <fredrik.amcoff@gmail.com>
2025-12-09 10:53:02 +0100

    simulation function

--- Assignment 2A.R
+++ Assignment 2A.R
@@ -62,24 +62,47 @@ fwlsFun <- function(data, trueVar){
   return(beta_fwls)
 }
 
-
+# Data generation function
+# n: number of observations
+# lambda: error variance weight
+# return: n x 2 matrix with x- and y-values
 DataFun <- function(n, lambda) {
-  x <- runif(n, 0, 2)
-  var_vector <- exp(x*lambda)
-  epsilon_vector <- numeric(n)
-  for (i in seq_along(var_vector)) {
-    var <- var_vector[[i]]
-    epsilon <- rnorm(1, 0, sqrt(var))
-    epsilon_vector[[i]] <- epsilon
+  x <- runif(n, 0, 2)  # x-values, U(0,2)-distributed
+  var_vector <- exp(x*lambda)  # vector with error variances (dependent on x and lambda)
+  epsilon_vector <- numeric(n)  # placeholder vector for error terms
+  for (i in seq_along(var_vector)) {  # sample N(0, exp(x_i*sigma_i))-distributed error terms
+    var <- var_vector[[i]]  # variance
+    epsilon <- rnorm(1, 0, sqrt(var))  # error term
+    epsilon_vector[[i]] <- epsilon  # append error term to vector
   }
   beta <- 2
-  y <- beta*x + epsilon_vector
-  output_matrix <- cbind(y, x)
+  y <- beta*x + epsilon_vector  # calculate y-values
+  output_matrix <- cbind(y, x)  # combine x and y to matrix
   return(output_matrix)
-
-
-# return(an n x 2 matrix or data frame)
 }
+
+SimFun <- function(n, sim_reps, seed, lambda) {
+  set.seed(seed)
+  ols_vector <- numeric(sim_reps)
+  wls_vector <- numeric(sim_reps)
+  fwls_vector_true <- numeric(sim_reps)
+  fwls_vector_false <- numeric(sim_reps)
+  for (i in 1:sim_reps) {
+    print(sprintf("Processing simulation %d", i))
+    data <- DataFun(n, lambda)
+    ols <- olsFun(data)
+    wls <- wlsFun(data, lambda=lambda)
+    fwls_true <- fwlsFun(data, trueVar=TRUE)
+    fwls_false <- fwlsFun(data, trueVar=FALSE)
+    ols_vector[[i]] <- ols[[2]]
+    wls_vector[[i]] <- wls[[2]]
+    fwls_vector_true[[i]] <- fwls_true[[2]]
+    fwls_vector_false[[i]] <- fwls_false[[2]]
+  }
+  return(c(var(ols_vector), var(wls_vector) ,var(fwls_vector_true) , var(fwls_vector_false)))
+# return(Vector of four variance estimates)
+}
+
 
 
 testData <- cbind( c(0.62, 0.18, 3.92, 0.80, -5.15),
@@ -89,6 +112,7 @@ testData <- cbind( c(0.62, 0.18, 3.92, 0.80, -5.15),
 #wlsFun(data = testData, lambda = 2),
 #fwlsFun(data = testData, trueVar = TRUE),
 #fwlsFun(data = testData, trueVar = FALSE), use.names = F)
-data <- DataFun(100, 2)
+#data <- DataFun(100, 2)
+print(SimFun(100, 100, 123, 2))
 
 
